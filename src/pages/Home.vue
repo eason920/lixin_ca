971bd38aca8e6723b4bccb747e90f405c633b337
eason920 <eason920@gmail.com>
2024-09-09 23:49:47 +0800

    svg ani

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,6 +12,7 @@
   <Nav v-if="info.navList.length > 0" />
   <div class="home overflow-hidden font-['Noto_Sans_TC',sans-serif]">
     <S1 />
+    <S2 />
     <S4 />
     <S5 />
     <S6 />
@@ -72,6 +73,7 @@ img {
 <script setup>
 import info from "@/info"
 import S1 from "@/section/s1.vue"
+import S2 from "@/section/s2.vue"
 import S4 from "@/section/s4.vue"
 import S5 from "@/section/s5.vue"
 import S6 from "@/section/s6.vue"
