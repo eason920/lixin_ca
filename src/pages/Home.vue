8c1a26cf7698d84dc8c509b72d982bfd838aa251
eason920 <eason920@gmail.com>
2024-09-11 22:13:26 +0800

    b2 new + b3 new + >> b4 change .. <<

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,9 +10,10 @@
   </div>
   <!--loading end-->
   <Nav v-if="info.navList.length > 0" />
-  <div class="home overflow-hidden font-['Noto_Sans_TC',sans-serif]">
+  <div class="home overflow-hidden font-['Noto_Sans_TC',sans-serif] bg-[#ffffff]">
     <S1 />
     <S2 />
+    <S3 />
     <S4 />
     <S5 />
     <S6 />
@@ -74,6 +75,7 @@ img {
 import info from "@/info"
 import S1 from "@/section/s1.vue"
 import S2 from "@/section/s2.vue"
+import S3 from "@/section/s3.vue"
 import S4 from "@/section/s4.vue"
 import S5 from "@/section/s5.vue"
 import S6 from "@/section/s6.vue"
