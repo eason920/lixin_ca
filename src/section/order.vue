09491fc81d3e9bea2c27d13f62bdfa1320b7f091
xcatx <xcatx796@gmail.com>
2024-11-12 16:53:05 +0800

    開啟地圖

--- src/section/order.vue
+++ src/section/order.vue
@@ -446,7 +446,7 @@ import HR from '@/section/public/hr.vue'
 //
 import Policy from "@/section/form/policy.vue"
 import ContactInfo from "@/section/form/contactInfo.vue"
-// import Map from "@/section/form/map.vue"
+import Map from "@/section/form/map.vue"
 import HouseInfo from "@/section/form/houseInfo.vue"
 
 import info from "@/info"
